1993f48154cdb4dd6bdd55eeb0accd2777ea08d4
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-03-14 23:16:52 +0100

    Added views

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Questions
+
+admin.site.register(Questions)
--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/result/', views.ResultView.as_view(), name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
